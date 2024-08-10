269f9447bf69337bcbe09cfa9837c428a55f9256
Maadeha shaikh <maadeham@gmail.com>
2024-08-10 14:50:08 +0500

    done

--- src/Components/LeftScroller/RightScroller.js
+++ src/Components/LeftScroller/RightScroller.js
@@ -2,27 +2,27 @@ import React from 'react'
 import  { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import 'swiper/css';
-import './style.css'
+// import 'swiper/css';
+// import './style.css';
 
 import { Autoplay } from 'swiper/modules';
 const RightScroller = () => {
   return (
-    <div className='w-[70%] h-full ml-4'>
-        <Swiper
-       direction={'vertical'}
-       slidesPerView={'auto'}
-       freeMode={true}
-       autoplay={true}
-       scrollbar={{ draggable: true }}
-       autoplay={{
-         delay: 2500,
-         disableOnInteraction: false,
-       }}
-       loop={true}
-       modules={[Autoplay]}
+    <div className='w-[70%] h-full ml-4 bg-green-500 '>
+        <Swiper className='w-50% bg-orange-500 swiper2'
+      //  direction={'vertical'}
+      //  slidesPerView={'auto'}
+      //  freeMode={true}
+      //  autoplay={true}
+      //  scrollbar={{ draggable: true }}
+      //  autoplay={{
+      //    delay: 2500,
+      //    disableOnInteraction: false,
+      //  }}
+      //  loop={true}
+      //  modules={[Autoplay]}
       >
-        <SwiperSlide className='bg-blue-400 w-full h-full text-white font-mono '>
+        <SwiperSlide className='swiperslide2'>
         <p>
         As the CEO of XYZ Company, I am incredibly proud of the journey we have embarked upon. From our humble beginnings, we set out with a vision to revolutionize the industry through innovation and dedication.
         </p>          
